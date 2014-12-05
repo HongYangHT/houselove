f404d7a1fa04cba7cbfb84ae0a44e076c93ec855
superuser <superuser@xinyunlian06>
2014-12-05 13:45:34 +0800

    full house deploy

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		less:{
+			compileLogin: {
+				option:{ },
+				files: {
+					'public/css/login.css':'public/less/login.less'
+				}
+			},
+			compileMain: {
+				options: { },
+				files: {
+					
+				}
+			},
+			minify : {
+                options : {
+                    cleancss : true,
+                    report : 'min',
+                    compress : true,
+                    cleancssOptions : {
+                        keepSpecialComments : 0,
+                        keepBreaks : false
+                    }
+                },
+                files : {
+                	'public/css/login.min.css':'public/css/login.css'
+                }
+            }
+		},
+		
+		watch:{
+			scripts: {
+			   files:['public/less/*.less','public/less/lib/*.less'],
+			   tasks:['less']
+			} 
+		
+		}
+	});	
+	
+	grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', [ 'less', 'watch' ]);
+    grunt.registerTask('buildAll', [ 'less', 'requirejs', 'uglify' ]);
+
+}
