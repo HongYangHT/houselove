6bd954be13fcf20d844f3e6a32d9feb8c5c8436a
Samhong <Samhong@WSWINCNHZ0771.hengtiansoft.com>
2014-12-11 17:30:06 +0800

    chat html add

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,8 @@ module.exports = function(grunt){
 				option:{ },
 				files: {
 					'public/css/login.css':'public/less/login.less',
-					'public/css/home.css':'public/less/home.less'
+					'public/css/home.css':'public/less/home.less',
+					'public/css/chat.css':'public/less/chat.less'
 				}
 			},
 			compileMain: {
@@ -28,7 +29,8 @@ module.exports = function(grunt){
                 	'public/css/lib/bootstrap-datetimepicker.min.css':'public/css/lib/bootstrap-datetimepicker.css',
                 	'public/css/lib/datepicker.min.css':'public/css/lib/datepicker.css',
                 	'public/css/login.min.css':'public/css/login.css',
-                	'public/css/home.min.css':'public/css/home.css'
+                	'public/css/home.min.css':'public/css/home.css',
+                	'public/css/chat.min.css':'public/css/chat.css'
                 }
             }
 		},
