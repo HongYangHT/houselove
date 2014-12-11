70402915e89a9579102acad50462e5001f6ab02e
Samhong <Samhong@WSWINCNHZ0771.hengtiansoft.com>
2014-12-11 15:49:16 +0800

    better home html

--- public/js/home.js
+++ public/js/home.js
@@ -1,9 +1,31 @@
 /**
+ * @version 0.0.1
  * @author hongyang
- * @description this is a home js
+ * @description handle the event in home.html
  * 
  */
 (function($){
-	var 
-
+	var $panelHeading = $('.panel-heading');
+	var $pModal = $('.p-modal');
+	var $pBadge = $('.p-badge');
+	
+	$panelHeading.on('click',function(e){
+		var $this = $(this);
+		$this.find('.switch-icon,.fa-angle-down,.fa-angle-up').toggleClass('fa-angle-down').toggleClass('fa-angle-up');
+	});
+	
+	$pBadge.on('click',function(e){
+		animateInOut();
+	});
+	
+	function animateInOut(){
+		$pModal.each(function(i,n){
+			var $n = $(n);
+//			$n.toggleClass('hidden');
+			$n.animate({height: 'toggle', opacity: 'toggle'},"normal",function(){
+				$n.toggleClass('hidden');
+			});
+		});		
+	}
+	
 })(jQuery)
